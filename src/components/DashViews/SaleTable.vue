8776f143b1f500aa286624f82da9d8ae18ac6301
Luan Henrique <luan.henriqueepp@gmail.com>
2020-04-03 10:22:25 -0300

    Fix total sale

--- src/components/DashViews/SaleTable.vue
+++ src/components/DashViews/SaleTable.vue
@@ -448,7 +448,7 @@
         } else {
           const user = localStorage.getItem('user')
           this.editedItem.user_id = user
-          this.editedItem.total_purchase = this.editedItem.quantity * this.editedItem.price
+          this.editedItem.total_sale = this.editedItem.quantity * this.editedItem.price
           let tableItem = this.editedItem
           this.data.push(this.editedItem)
           let endpoint = `sale`
